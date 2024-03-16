e5e71e247c918574c6c5c7919cacfd1aefdbb2b0
lufy20105 <144891766+lufy20106@users.noreply.github.com>
2024-03-16 12:31:22 +0700

    a

--- math/index.js
+++ math/index.js
@@ -1,29 +1,29 @@
-alert('Hế lô')
+alert('Wel coems')
 function add() {
     var num1 = parseFloat(document.getElementById("num1").value)
     var num2 = parseFloat(document.getElementById("num2").value)
     var result= num1 + num2
-    document.getElementById("result").innerHTML = "Kết quả: " + result
+    document.getElementById("result").innerHTML = "Result: " + result
 }
 function subtract() {
     var num1 = parseFloat(document.getElementById("num1").value)
     var num2 = parseFloat(document.getElementById("num2").value)
     var result= num1 - num2
-    document.getElementById("result").innerHTML = "Kết quả: " + result
+    document.getElementById("result").innerHTML = "Result: " + result
 }
 function multiply() {
     var num1 = parseFloat(document.getElementById("num1").value)
     var num2 = parseFloat(document.getElementById("num2").value)
     var result= num1 * num2
-    document.getElementById("result").innerHTML = "Kết quả: " + result
+    document.getElementById("result").innerHTML = "Result: " + result
 }
 function divide() {
     var num1 = parseFloat(document.getElementById("num1").value)
     var num2 = parseFloat(document.getElementById("num2").value)
     if (num2 === 0) {
-        document.getElementById("result").innerHTML = ":V chia số 0 à :V"
+        document.getElementById("result").innerHTML = "Why 0"
     } else {
         var result = num1 / num2;
-        document.getElementById("result").innerHTML = "Kết quả: " + result
+        document.getElementById("result").innerHTML = "Result: " + result
     }
 }
